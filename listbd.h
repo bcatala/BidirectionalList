35c0dc8a4ce13ad915f26a5aa8d789e755d03e4f
bcatala <bernat.catalai@students.salle.url.edu>
2019-06-13 17:47:13 +0200

    Creation listbd.h
    Create listbd.h with headers of .c functions

---
+++ listbd.h
@@ -0,0 +1,24 @@
+#ifndef _LISTA_BID_H
+#define _LISTA_BID_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ListaBid LISTABID_crea ();
+ListaBid LISTABID_anadirDerecha (ListaBid l, int e);
+ListaBid LISTABID_anadirIzquierda (ListaBid l, int e);
+ListaBid LISTABID_anadirOrdenado (ListaBid l, int e);
+ListaBid LISTABID_eliminar (ListaBid l);
+int      LISTABID_consultar (ListaBid l);
+int      LISTABID_vacia (ListaBid l);
+int      LISTABID_final (ListaBid l);
+int      LISTABID_principio (ListaBid l);
+ListaBid LISTABID_irPrincipio (ListaBid l);
+ListaBid LISTABID_irFinal (ListaBid l);
+ListaBid LISTABID_avanzar (ListaBid l);
+ListaBid LISTABID_retroceder (ListaBid l);
+ListaBid LISTABID_destruye (ListaBid l);
+ListaBid LISTABID_vaciar (ListaBid l) ;
+
+#endif
