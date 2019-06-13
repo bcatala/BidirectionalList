b469a8a21450e1d9c2c67cf61b9c02c770a8671a
bcatala <bernat.catalai@students.salle.url.edu>
2019-06-13 17:43:48 +0200

    FEAT-Creation listbd.c
    Creation listbd.c, with the headers of the functions that we are going to implement.

---
+++ listbd.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+
+ListaBid LISTABID_crea () {
+	
+	return l;
+}
+
+
+ListaBid LISTABID_anadirDerecha (ListaBid l, int e) {
+	
+
+	return l;
+}
+
+ListaBid LISTABID_anadirIzquierda (ListaBid l, int e) {
+	
+	return l;
+}
+
+ListaBid LISTABID_anadirOrdenado (ListaBid l, int e) {
+
+	
+	
+	return l;
+}
+
+
+ListaBid LISTABID_eliminar (ListaBid l) {
+	
+	return l;
+}
+
+int      LISTABID_consultar (ListaBid l) {
+	
+}
+
+int      LISTABID_vacia (ListaBid l) {
+	
+}
+
+int      LISTABID_final (ListaBid l) {
+	
+}
+
+int      LISTABID_principio (ListaBid l) {
+	
+}
+
+ListaBid LISTABID_irPrincipio (ListaBid l) {
+	
+	
+	return l;
+}
+
+ListaBid LISTABID_irFinal (ListaBid l) {
+	
+	
+	return l;
+}
+
+ListaBid LISTABID_avanzar (ListaBid l) {
+	
+	
+	return l;
+}
+
+ListaBid LISTABID_retroceder (ListaBid l) {
+	
+	
+	return l;
+}
+
+ListaBid LISTABID_destruye (ListaBid l) {
+	
+	return l;
+}
+
+ListaBid LISTABID_vaciar (ListaBid l) {
+
+	
+	
+	return l;
+}
+
+
+
+
+ 
